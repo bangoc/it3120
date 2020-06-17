cc00e851350b4f3b29cbe94f62743f9a94f25c91
Nguyen Ba Ngoc <nguyen_bn@hotmail.com>
2020-06-17 22:28:48 +0700

    Ví dụ minh họa các lớp DAM và CSDL

---
+++ demo/DAM/src/main/java/App.java
@@ -0,0 +1,61 @@
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+import javax.sql.DataSource;
+
+import org.h2.jdbcx.JdbcDataSource;
+
+public class App {
+  private static final String DB_URL = "jdbc:h2:~/tmp";
+
+  public static void main(final String[] args) throws Exception {
+    final DataSource dataSource = createDataSource();
+    createSchema(dataSource);
+    final CustomerDAM customerDAM = new CustomerDAM(dataSource);
+    customerDAM.add(new Customer(1, "Nguyen", "Van A"));
+    customerDAM.add(new Customer(2, "Tran", "Thi B"));
+    customerDAM.add(new Customer(3, "Bui", "Xuan C"));
+    System.out.println("customerDAM.getAllCustomers(): ");
+    try (Stream<Customer> customerStream = customerDAM.getAll()) {
+      customerStream.forEach((customer) -> System.out.println(customer));
+    }
+    System.out.println("customerDAM.getCustomerById(2): " + customerDAM.getById(2));
+    final Customer customer = new Customer(4, "Vo", "Van D");
+    customerDAM.add(customer);
+    System.out.println("customerDAM.getAllCustomers(): " + customerDAM.getAll());
+    customer.setFirstName("La");
+    customer.setLastName("Van F");
+    customerDAM.update(customer);
+    System.out.println("customerDAM.getAllCustomers(): ");
+    try (Stream<Customer> customerStream = customerDAM.getAll()) {
+      customerStream.forEach((cust) -> System.out.println(cust));
+    }
+    customerDAM.delete(customer);
+    System.out.println("customerDAM.getAllCustomers(): " + customerDAM.getAll());
+    deleteSchema(dataSource);
+  }
+
+  private static void deleteSchema(DataSource dataSource) throws SQLException {
+    try (Connection connection = dataSource.getConnection();
+        Statement statement = connection.createStatement()) {
+      statement.execute(CustomerSchemaSql.DELETE_SCHEMA_SQL);
+    }
+  }
+
+  private static void createSchema(DataSource dataSource) throws SQLException {
+    try (Connection connection = dataSource.getConnection();
+        Statement statement = connection.createStatement()) {
+      statement.execute(CustomerSchemaSql.CREATE_SCHEMA_SQL);
+    }
+  }
+
+  private static DataSource createDataSource() {
+    JdbcDataSource dataSource = new JdbcDataSource();
+    dataSource.setURL(DB_URL);
+    return dataSource;
+  }
+}
---
+++ demo/DAM/src/main/java/CustomerDAM.java
@@ -0,0 +1,113 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Optional;
+import java.util.Spliterator;
+import java.util.Spliterators;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+import javax.sql.DataSource;
+
+public class CustomerDAM {
+
+  private final DataSource dataSource;
+
+  public CustomerDAM(DataSource dataSource) {
+    this.dataSource = dataSource;
+  }
+
+  public Stream<Customer> getAll() throws Exception {
+    Connection connection = getConnection();
+    PreparedStatement statement = connection.prepareStatement("SELECT * FROM CUSTOMERS"); // NOSONAR
+    ResultSet resultSet = statement.executeQuery(); // NOSONAR
+    return StreamSupport.stream(new Spliterators.AbstractSpliterator<Customer>(Long.MAX_VALUE,
+        Spliterator.ORDERED) {
+
+      @Override
+      public boolean tryAdvance(Consumer<? super Customer> action) {
+        try {
+          if (!resultSet.next()) {
+            return false;
+          }
+          action.accept(createCustomer(resultSet));
+          return true;
+        } catch (SQLException e) {
+          throw new RuntimeException(e); // NOSONAR
+        }
+      }
+    }, false).onClose(() -> mutedClose(connection, statement, resultSet));
+  }
+
+  private Connection getConnection() throws SQLException {
+    return dataSource.getConnection();
+  }
+
+  private void mutedClose(Connection connection, PreparedStatement statement, ResultSet resultSet) {
+    try {
+      resultSet.close();
+      statement.close();
+      connection.close();
+    } catch (SQLException e) {
+      System.out.println("Exception thrown " + e.getMessage());
+    }
+  }
+
+  private Customer createCustomer(ResultSet resultSet) throws SQLException {
+    return new Customer(resultSet.getInt("ID"),
+        resultSet.getString("FNAME"),
+        resultSet.getString("LNAME"));
+  }
+
+  public Optional<Customer> getById(int id) throws Exception {
+
+    ResultSet resultSet = null;
+
+    Connection connection = getConnection();
+    PreparedStatement statement =
+        connection.prepareStatement("SELECT * FROM CUSTOMERS WHERE ID = ?");
+
+    statement.setInt(1, id);
+    resultSet = statement.executeQuery();
+    if (resultSet.next()) {
+      return Optional.of(createCustomer(resultSet));
+    } else {
+      return Optional.empty();
+    }
+  }
+
+  public boolean add(Customer customer) throws Exception {
+    if (getById(customer.getId()).isPresent()) {
+      return false;
+    }
+
+    Connection connection = getConnection();
+    PreparedStatement statement =
+        connection.prepareStatement("INSERT INTO CUSTOMERS VALUES (?,?,?)");
+    statement.setInt(1, customer.getId());
+    statement.setString(2, customer.getFirstName());
+    statement.setString(3, customer.getLastName());
+    statement.execute();
+    return true;
+  }
+
+  public boolean update(Customer customer) throws Exception {
+    Connection connection = getConnection();
+    PreparedStatement statement =
+        connection.prepareStatement("UPDATE CUSTOMERS SET FNAME = ?, LNAME = ? WHERE ID = ?");
+    statement.setString(1, customer.getFirstName());
+    statement.setString(2, customer.getLastName());
+    statement.setInt(3, customer.getId());
+    return statement.executeUpdate() > 0;
+  }
+
+  public boolean delete(Customer customer) throws Exception {
+    Connection connection = getConnection();
+    PreparedStatement statement =
+        connection.prepareStatement("DELETE FROM CUSTOMERS WHERE ID = ?");
+    statement.setInt(1, customer.getId());
+    return statement.executeUpdate() > 0;
+  }
+}
